c3373edb9f9de21f6cbcbe7a0dae132a3c7dde90
gajun755 <48642457+gajun755@users.noreply.github.com>
2023-05-20 17:33:42 +0530

    added sample spring boot project

---
+++ SpringBootTest/src/test/java/com/dedication/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
